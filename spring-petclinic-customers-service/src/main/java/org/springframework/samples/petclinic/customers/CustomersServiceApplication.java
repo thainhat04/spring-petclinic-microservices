e1f3dcf3f87422a5d32b9a648be45aa52547661b
nhattran04 <nhattran@phygitallabs.io>
2025-07-06 00:29:45 +0700

    test: ci v4

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -31,5 +31,6 @@ public class CustomersServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomersServiceApplication.class, args);
+		System.out.println("CustomersServiceApplication started");
 	}
 }
